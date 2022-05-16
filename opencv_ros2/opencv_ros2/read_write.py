3c23cb636edbc92804621e81faad4a4be020872f
Jeffrey Tan <i@jeffreytan.org>
2022-05-16 23:25:13 +0800

    日本語コメント追加

--- opencv_ros2/opencv_ros2/read_write.py
+++ opencv_ros2/opencv_ros2/read_write.py
@@ -1,19 +1,19 @@
-# import the cv2 library
+# cv2 ライブラリをインポートする
 import cv2
 
-# The function cv2.imread() is used to read an image.
+# 画像を読み込むには，関数 cv2.imread() を用います．
 img = cv2.imread('fruits.jpg')
 img_grayscale = cv2.imread('fruits.jpg',cv2.IMREAD_GRAYSCALE)
 
-# The function cv2.imshow() is used to display an image in a window.
+# 画像をウィンドウに表示するには，関数 cv2.imshow() を用います．
 cv2.imshow('image',img)
 cv2.imshow('grayscale image',img_grayscale)
 
-# The function cv2.imwrite() is used to write an image.
+# 画像の書き込みには，関数 cv2.imwrite() を用います．
 cv2.imwrite('fruits_grayscale.jpg',img_grayscale)
 
-# waitKey() waits for a key press to close the window and 0 specifies indefinite loop
+# waitKey()はキーが押されるのを待ってウィンドウを閉じる、0は不定ループを指定する．
 cv2.waitKey(0)
 
-# cv2.destroyAllWindows() simply destroys all the windows we created.
+# cv2.destroyAllWindows() は、作成したすべてのウィンドウを単純に破棄します．
 cv2.destroyAllWindows()
