9e611aed70d249cf5b03ae99c6e29d25279e6eba
Jeffrey Tan <i@jeffreytan.org>
2022-05-18 03:59:36 +0800

    aruco_node_tf.py更新

--- opencv_ros2/opencv_ros2/aruco_node_tf.py
+++ opencv_ros2/opencv_ros2/aruco_node_tf.py
@@ -28,20 +28,26 @@ class ArucoNodeTF(rclpy.node.Node):
         self.declare_parameter("camera_frame", None)
         self.declare_parameter("aruco_marker_name", "aruco_marker")
      
-        self.marker_size = self.get_parameter("marker_size").get_parameter_value().double_value
+        self.marker_size = self.get_parameter(
+            "marker_size").get_parameter_value().double_value
         dictionary_id_name = self.get_parameter(
             "aruco_dictionary_id").get_parameter_value().string_value
-        image_topic = self.get_parameter("image_topic").get_parameter_value().string_value
-        info_topic = self.get_parameter("camera_info_topic").get_parameter_value().string_value
-        self.camera_frame = self.get_parameter("camera_frame").get_parameter_value().string_value
-        self.aruco_marker_name = self.get_parameter("aruco_marker_name").get_parameter_value().string_value
+        image_topic = self.get_parameter(
+            "image_topic").get_parameter_value().string_value
+        info_topic = self.get_parameter(
+            "camera_info_topic").get_parameter_value().string_value
+        self.camera_frame = self.get_parameter(
+            "camera_frame").get_parameter_value().string_value
+        self.aruco_marker_name = self.get_parameter(
+            "aruco_marker_name").get_parameter_value().string_value
 
         try:
             dictionary_id = cv2.aruco.__getattribute__(dictionary_id_name)
             if type(dictionary_id) != type(cv2.aruco.DICT_5X5_100):
                 raise AttributeError
         except AttributeError:
-            self.get_logger().error("bad aruco_dictionary_id: {}".format(dictionary_id_name))
+            self.get_logger().error(
+                "bad aruco_dictionary_id: {}".format(dictionary_id_name))
             options = "\n".join([s for s in dir(cv2.aruco) if s.startswith("DICT")])
             self.get_logger().error("valid options: {}".format(options))
 
@@ -91,21 +97,27 @@ class ArucoNodeTF(rclpy.node.Node):
         markers.header.stamp = img_msg.header.stamp
         pose_array.header.stamp = img_msg.header.stamp
 
-        corners, marker_ids, rejected = cv2.aruco.detectMarkers(cv_image,
-                                                                self.aruco_dictionary,
-                                                                parameters=self.aruco_parameters)
+        corners, marker_ids, rejected = cv2.aruco.detectMarkers(
+            cv_image,
+            self.aruco_dictionary,
+            parameters=self.aruco_parameters)
+
         if marker_ids is not None:
             
             cv2.aruco.drawDetectedMarkers(cv_image, corners, marker_ids)
  
             if cv2.__version__ > '4.0.0':
-                rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners,
-                                                                      self.marker_size, self.intrinsic_mat,
-                                                                      self.distortion)
+                rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+                    corners,
+                    self.marker_size,
+                    self.intrinsic_mat,
+                    self.distortion)
             else:
-                rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(corners,
-                                                                   self.marker_size, self.intrinsic_mat,
-                                                                   self.distortion)
+                rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(
+                    corners,
+                    self.marker_size,
+                    self.intrinsic_mat,
+                    self.distortion)
             for i, marker_id in enumerate(marker_ids):
                 t = TransformStamped()
                 t.header.stamp = self.get_clock().now().to_msg()
@@ -132,7 +144,10 @@ class ArucoNodeTF(rclpy.node.Node):
                 
                 self.tfbroadcaster.sendTransform(t)
                 
-                cv2.aruco.drawAxis(cv_image, self.intrinsic_mat, self.distortion, rvecs[i], tvecs[i], 0.05)
+                cv2.aruco.drawAxis(
+                    cv_image, self.intrinsic_mat,
+                    self.distortion, rvecs[i],
+                    tvecs[i], 0.05)
 
                 pose.orientation.x = quat[0]
                 pose.orientation.y = quat[1]
