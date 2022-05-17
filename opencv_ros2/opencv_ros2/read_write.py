2645d3bee547576fe5ad22c32a82120423980385
Jeffrey Tan <i@jeffreytan.org>
2022-05-17 11:51:06 +0800

    video_subscriber.py更新

--- opencv_ros2/opencv_ros2/read_write.py
+++ opencv_ros2/opencv_ros2/read_write.py
@@ -1,19 +1,13 @@
-# cv2 ライブラリをインポートする
 import cv2
 
-# 画像を読み込むには，関数 cv2.imread() を用います．
 img = cv2.imread('fruits.jpg')
 img_grayscale = cv2.imread('fruits.jpg',cv2.IMREAD_GRAYSCALE)
 
-# 画像をウィンドウに表示するには，関数 cv2.imshow() を用います．
 cv2.imshow('image',img)
 cv2.imshow('grayscale image',img_grayscale)
 
-# 画像の書き込みには，関数 cv2.imwrite() を用います．
 cv2.imwrite('fruits_grayscale.jpg',img_grayscale)
 
-# waitKey()はキーが押されるのを待ってウィンドウを閉じる、0は不定ループを指定する．
 cv2.waitKey(0)
 
-# cv2.destroyAllWindows() は、作成したすべてのウィンドウを単純に破棄します．
 cv2.destroyAllWindows()
--- opencv_ros2/opencv_ros2/video_subscriber.py
+++ opencv_ros2/opencv_ros2/video_subscriber.py
@@ -1,10 +1,10 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
-from sensor_msgs.msg import Image # Image is the message type
+from sensor_msgs.msg import Image
 
-import cv2 # OpenCV library
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import cv2
+from cv_bridge import CvBridge
 
 
 class VideoSubscriber(Node):
@@ -13,41 +13,29 @@ class VideoSubscriber(Node):
         super().__init__('video_subscriber')
         self.subscription = self.create_subscription(
             Image,
-            '/image_raw', # webcam topic
-            #'/camera/color/image_raw', # realsense camera topic
-            self.listener_callback,
+            'image_raw', # usb_cam topic
+            self.image_callback,
             qos_profile_sensor_data)
+
+        self.publisher = self.create_publisher(
+            Image,
+            'result', 10)
         
-        # Used to convert between ROS and OpenCV images
         self.br = CvBridge()
 
-    def listener_callback(self, data):
-        
-        # Display the message on the console
-        self.get_logger().info('Receiving image')
-        
-        # Convert ROS Image message to OpenCV image
+    def image_callback(self, data):
         frame = self.br.imgmsg_to_cv2(data, "bgr8")
-        
-        # Display image
-        cv2.imshow("camera", frame)
-        
-        cv2.waitKey(1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, result = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+        result_msg = self.br.cv2_to_imgmsg(result, 'passthrough')
+        self.publisher.publish(result_msg)
 
 
-def main(args=None):
-    rclpy.init(args=args)
-
+def main():
+    rclpy.init()
     video_subscriber = VideoSubscriber()
-
-    rclpy.spin(video_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    video_subscriber.destroy_node()
+    try:
+        rclpy.spin(video_subscriber)
+    except KeyboardInterrupt:
+        pass
     rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
