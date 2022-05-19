2c3a302f62aaa1248881dbeeeb217044e04b736d
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2022-05-19 16:48:49 +0900

    微修正

--- opencv_ros2/opencv_ros2/canny_edge_detection.py
+++ opencv_ros2/opencv_ros2/canny_edge_detection.py
@@ -5,7 +5,6 @@ from sensor_msgs.msg import Image
 
 import cv2
 from cv_bridge import CvBridge
-import numpy as np
 
 
 class CannyEdgeDetection(Node):
@@ -17,24 +16,24 @@ class CannyEdgeDetection(Node):
             '/image_raw',
             self.image_callback,
             qos_profile_sensor_data)
-        
+
         self.publisher = self.create_publisher(
-            Image, 
+            Image,
             'edges_result', 10)
 
         self.br = CvBridge()
 
     def image_callback(self, data):
-        frame = self.br.imgmsg_to_cv2(data, "bgr8")
+        frame = self.br.imgmsg_to_cv2(data, 'bgr8')
         frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        edges = cv2.Canny(frame_grayscale,100,200)
 
-        edges_result = self.br.cv2_to_imgmsg(edges, "passthrough")
+        edges = cv2.Canny(frame_grayscale, threshold1=100, threshold2=200)
+
+        edges_result = self.br.cv2_to_imgmsg(edges, 'passthrough')
         self.publisher.publish(edges_result)
 
-        cv2.imshow("Original Image", frame)
-        cv2.imshow("Canny Edge Detection", edges)
+        cv2.imshow('Original Image', frame)
+        cv2.imshow('Canny Edge Detection', edges)
 
         cv2.waitKey(1)
 
